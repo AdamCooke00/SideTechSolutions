3a6d31bbaaf53e070be44fde52a5df11af3e3004
AdamCooke00 <AdamCooke00@gmail.com>
2021-10-07 00:36:32 -0400

    add tag for favicon, doesn't seem to work unforunately

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="description" content="A platform to assist students and landlords find each other. Trialing in Kingston Ontario, Student Housing Hub aims to
         assist Queen's University Students find housing rental accomodations by offering a simpler alternative. Includes house filtering functionality."></meta>
-        <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html" charset="utf-8" />
         <meta name="viewport" content="width=device-width" initial-scale="1" />
         <body>
           <Main />
