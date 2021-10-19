10683babd735550301ecd4ec95f2a35f4941ca27
AdamCooke00 <AdamCooke00@gmail.com>
2021-10-19 11:23:36 -0400

    update safety page

--- pages/safety.js
+++ pages/safety.js
@@ -1,11 +1,12 @@
 import Head from "next/head"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import Link from "next/link"
 
 export default function Safety() {
 
   return (
-    <div>
+    <div className="entire-page">
       <Head>
         <title>Student Housing Hub</title>
       </Head>
@@ -13,15 +14,19 @@ export default function Safety() {
       <div className="aboutpage">
         <h1 className="aboutpagetitle">Safety</h1>
         <div className="aboutpageparagraphs">
-          <p>The goal of Student Housing Hub is to be an all-in-one platform through which landlords can list their rentals and students can filter through them and reach out
-            to the landlord easier.</p>
-          <p>I am currently a Queen's University student and have had first-hand experience in the difficulties of the student housing hunt.</p>
-          <p>Currently, finding the right house as a student is difficult as there are not many ways to filter listings. Add on the time commitment school is with the time it takes
-            to find the right place to live, one can become stressed very quickly. Student Housing Hub aims to decrease the amount of time and stress finding a house takes and causes.</p>
-          <p>For this 2021-2022 school year, Student Housing Hub's goal is to produce an Alpha version of the platform. This version aims to allow landlords to post potential rentals,
-            and allows students to filter through these rentals with the intention of reaching out to the landlord of the unit they wish to rent.</p>
-          <p>In time, Student Housing Hub aims to add an ease of communication feature that allows communicating through the platform and an accompanying mobile application.</p>
-          <p>The target for this Alpha Version will be Queen's University in Kingston, Ontario.</p>
+          <p>Safety is crucial when it comes to finding a place to live, especially as a student. Even though you may be new to ownership and renting, its imperative to understand that
+            you are not allowed to be taken advantage of.</p>
+          <p>Student Housing Hub has outlined below a list of things to consider <strong>BEFORE</strong> signing a rental unit.</p>
+          <ul>
+            <li>Is the property up to code? When touring make sure you ask the landlord about anything you see that seems unsafe or hazardous.</li>
+            <li>If you are able to, get into contact with the previous tenants. Ask them if there is anything you should know before signing.</li>
+            <li>Do your due diligence and search for reviews on your potential landlord. Landlords are not your friend and do not always have your best interests in mind.</li>
+            <li><strong>READ THE LEASE AGGREMENT.</strong> It may be long and tedious, but you only need to do it once. It is common to find mistakes in the lease that need
+            to be fixed before you sign. Also, you will now know what you can and cannot due per the aggrement.</li>
+          </ul>
+          <p>*Student Housing Hub is not responsible for any conflict that may arise between landlord and tenant.*</p>
+          <p>This page will be updated through time as seen fit to equip students with more tools to assist them in safely finding a rental.</p>
+          <p>Consult the <Link href="/contact"><strong>CONTACT PAGE</strong></Link> for an questions or inquires.</p>
         </div>
       </div>
       <Footer/>
