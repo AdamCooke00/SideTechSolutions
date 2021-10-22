bf3a10521f491ee9e5195899d9ef21fa758629a1
AdamCooke00 <AdamCooke00@gmail.com>
2021-10-22 00:05:33 -0400

    capitilize landlord and comment analytics for future implementation

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app';
+// import 'firebase/analytics'
 import 'firebase/auth';
 import "firebase/firestore"
 import "firebase/storage"
@@ -10,7 +11,8 @@ const firebaseConfig = {
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID 
+    appId: process.env.NEXT_PUBLIC_APP_ID
+    // measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
 };
 
 try {
@@ -20,6 +22,7 @@ try {
         console.error('Firebase initialization error', err.stack)}
 }
 
+// const analytics = () => firebase.analytics();
 export const db = firebase.firestore();
 export const auth = firebase.auth();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
